fa0d0aa8d3ff41c9de02b1f61f113d527d5be3f9
sanarberkebayram <sanarberkebayram@gmail.com>
2025-08-25 23:32:03 +0300

    [Updated] Interactive mobile cursor bug

--- app/page.js
+++ app/page.js
@@ -8,7 +8,9 @@ import WorkSection from "./sections/WorkSection";
 export default function Home() {
     return (
         <main className="container mx-auto px-4 pt-8 md:pt-24 pb-8 md:pb-4 space-y-16 md:space-y-24 relative z-10 scroll-smooth">
-            <TargetCursor spinDuration={2} hideDefaultCursor={false} />
+            <div className="hidden md:block">
+                <TargetCursor spinDuration={2} hideDefaultCursor={false} />
+            </div>
             <HeroSection />
             <AboutSection />
             <WorkSection/>
